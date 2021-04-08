fa01426c9b361336740f7555ef759bc9c58c3896
smironescu <sergio.mironescu@marfeel.com>
2021-04-08 10:04:30 -0500

    removed expo dependecies and using bare react - tested iOS - working correctly - saving changes before configuring android devices

--- index.ts
+++ index.ts
@@ -2,9 +2,11 @@ if (__DEV__) {
     import('./src/js/Utils/common/reactotronConfig');
 }
 import { AppRegistry, Platform } from 'react-native';
+import 'react-native-gesture-handler';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 import App from './App';
 
-AppRegistry.registerComponent('moodem', () => App);
+AppRegistry.registerComponent('moodem', () => gestureHandlerRootHOC(App));
 
 if (Platform.OS === 'web') {
     const rootTag = document.getElementById('root') || document.getElementById('main');
